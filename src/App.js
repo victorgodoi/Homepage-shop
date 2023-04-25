79ffa9c5d7d220ecfbbca90aa2cb7f1dbf11a0e8
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2023-04-25 16:04:33 -0300

    Começo do site

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
-import styles from './App.module.scss';
-
 function App() {
   return (
-    <div className={styles.app}>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div id='page'>
+      <header>
+        <div className='conteudo'>
+          <img src='/images/Stylish.png' width='134' height='59' />
+        </div>
       </header>
     </div>
   );
