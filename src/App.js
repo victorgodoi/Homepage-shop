a390271efb4deef65a9fe44dcf3d987c8b492a17
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2023-04-26 15:46:29 -0300

    fazendo Header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ function App() {
     <div id='page'>
       <header>
         <div className='conteudo'>
-          <img src='/images/Stylish.png' width='134' height='59' />
+          <div>
+            <img src='/images/Stylish.png' width='134' height='59' />
+            <img src='/images/embaixoDoLogo.png' wdtch='133' height='20' />
+          </div>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Men</li>
+              <li>Women</li>
+              <li>Trends</li>
+              <li>Collections</li>
+              <li>Sale</li>
+              <li>Blog</li>
+              <li><img src='bx_bx-cart-alt.png' widtc='24' height='24' /></li>
+              <li><img src='bx_bx-shopping-bag.png' widtc='24' height='24' /></li>
+            </ul>
+          </nav>
         </div>
       </header>
     </div>
