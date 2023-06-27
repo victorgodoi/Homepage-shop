81027ff030f35ba19e774b677e1a8f0bad50096c
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2023-06-27 18:44:00 -0300

    Começando o Box1

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import styled from './App.module.scss';
 function App() {
   return (
     <div id={styled.page}>
-      <header>
-        <div className={styled.conteudo}>
+      <div className={styled.conteudo}>
+        <header>
           <div className={styled.divEsq}>
-            <img src='/images/Stylish.png' width='134' height='59' />
-            <img src='/images/embaixoDoLogo.png' wdtch='133' height='20' />
+            <img src='/images/Stylish.png' width='134' height='49' />
+            <img src='/images/embaixoDoLogo.png' width='133' height='13' />
           </div>
           <nav>
             <ul>
@@ -23,12 +23,19 @@ function App() {
               <li>Collections</li>
               <li>Sale</li>
               <li>Blog</li>
-              <li><img src='/images/carrinho.png' widtc='24' height='24' /></li>
-              <li><img src='/images/sacola.png' widtc='24' height='24' /></li>
+              <li><img src='/images/sacola.png' width='24' height='24' /></li>
+              <li><img src='/images/carrinho.png' width='24' height='24' /></li>
             </ul>
           </nav>
+        </header>
+        <div id={styled.box1}>
+          <img src='/images/FotoBox1.png' width='837' height='722' />
+          <div>
+            <img src='/images/Foto2Box1.png' width='808' height='342' />
+            <img src='/images/Foto3Box1.png' width='808' height='309' />
+          </div>
         </div>
-      </header>
+      </div>
     </div>
   );
 }
